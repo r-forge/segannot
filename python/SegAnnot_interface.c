db689604ba1534431d3da6a6825002cffbcb360e
tdhock (Toby Dylan Hocking) <tdhock5@gmail.com>
2017-09-13 13:00:19 +0000

    use PyArray_ISINTEGER instead of PyArray_Type
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/segannot@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- python/SegAnnot_interface.c
+++ python/SegAnnot_interface.c
@@ -1,35 +1,6 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 #include "SegAnnot.h"
-
-static PyObject *
-PrunedDP2Py(PyObject *self, PyObject *args){
-    PyArrayObject *signal;
-    int Kmax;
-    if(!PyArg_ParseTuple(args, "O!i",
-			 &PyArray_Type, &signal,
-			 &Kmax)){
-	return NULL;
-    }
-    if(PyArray_TYPE(signal)!=PyArray_DOUBLE){
-	PyErr_SetString(PyExc_TypeError,
-			"signal must be numpy.ndarray type double");
-	return NULL;
-    }
-    npy_intp n_signal = PyArray_DIM(signal, 0);
-    npy_intp n_path = n_signal * Kmax;
-    PyObject *path = PyArray_SimpleNew(1,&n_path,PyArray_INT);
-    int *pathA = (int*)PyArray_DATA(path);
-    double *signalA = (double*)PyArray_DATA(signal);
-    int status = PrunedDP(signalA, n_signal, Kmax, pathA);
-    if(status != 0){
-	PyErr_SetString(PyExc_TypeError,
-			"signal/kmax too small");
-	return NULL;
-    }
-    return path;
-}
- 
 static PyObject *
 SegAnnotBases2Py(PyObject *self, PyObject *args){
     PyArrayObject *signal, *base, *starts, *ends; //borrowed
@@ -41,22 +12,22 @@ SegAnnotBases2Py(PyObject *self, PyObject *args){
 	   )){
 	return NULL;
     }
-    if(PyArray_TYPE(signal)!=PyArray_DOUBLE){
+    if(!PyArray_ISFLOAT(signal)){
 	PyErr_SetString(PyExc_TypeError,
 			"signal must be numpy.ndarray type double");
 	return NULL;
     }
-    if(PyArray_TYPE(base)!=PyArray_INT){
+    if(!PyArray_ISINTEGER(base)){
 	PyErr_SetString(PyExc_TypeError,
 			"base must be numpy.ndarray type int");
 	return NULL;
     }
-    if(PyArray_TYPE(starts)!=PyArray_INT){
+    if(!PyArray_ISINTEGER(starts)){
 	PyErr_SetString(PyExc_TypeError,
 			"starts must be numpy.ndarray type int");
 	return NULL;
     }
-    if(PyArray_TYPE(ends)!=PyArray_INT){
+    if(!PyArray_ISINTEGER(ends)){
 	PyErr_SetString(PyExc_TypeError,
 			"ends must be numpy.ndarray type int");
 	return NULL;
